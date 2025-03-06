2eee885d7701e696394618d0e51f5521549f8de6
msherazi <76914453+msherazi@users.noreply.github.com>
2025-03-05 20:05:24 -0500

    const parameters initialization

--- klein-gordon.cpp
+++ klein-gordon.cpp
@@ -1,1 +1,11 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include <fstream>
+
+const int N_x = 100; //how many points there are on the 1D grid
+const double domain_size = 10.0; //size of the physical domain
+const double dx = domain_size/N_x; //distance between grid points on the physical domain (spatial step size)
+
+
+
