fc2d77f251dc0f70fa45914c2276382b0c865139
msherazi <76914453+msherazi@users.noreply.github.com>
2025-03-06 00:50:11 -0500

    initial conditions

--- klein-gordon.cpp
+++ klein-gordon.cpp
@@ -20,7 +20,17 @@ const double sigma = 0.5;  //gaussian width
 const double k = 3.0; //wave number
 
 
-void initial_conditions()
+void initial_conditions(std::vector<double>& phi, std::vector<double>& prev_phi)
+{
+    for(int i = 0; i < N_x; i++)
+    {
+        double x_i = i * dx; //physical coordinate which corresponds to the ith grid point
+        phi[i] = A * std::exp(-1 * std::pow((x_i - x_0), 2)/(2.0 * sigma * sigma)) * std::cos(k * x_i); //initialization of each field point based on gaussian wave packet
+        prev_phi[i] = phi[i]; //each future time step requires knowledge of the previous and present time steps 
+    }
+}
+
+void time_evolution(std::vector<double>& phi, std::vector<double>& prev_phi, std::vector<double>& prev_new)
 {
 
 }
